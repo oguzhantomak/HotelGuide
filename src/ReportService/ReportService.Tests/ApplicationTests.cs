d251965d06fcb507d7c2a86a2838341d732e3b42
Oğuzhan Tomak <oguzhantomak@gmail.com>
2024-12-01 19:57:16 +0300

    Add test projects
    Added new test projects for HotelManagementService and ReportService.
    Removed WeatherForecast class from both API projects.
    Enhanced test coverage with new unit tests for services, domain entities, and controllers.
    Updated solution file to include new test projects and configurations.

---
+++ src/ReportService/ReportService.Tests/ApplicationTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ReportService.API.Controllers;
+using ReportService.Application.Dtos;
+using ReportService.Application.Interfaces;
+
+namespace ReportService.Tests;
+
+public class ApplicationTests
+{
+    private readonly Mock<IReportService> _mockReportService;
+    private readonly ReportController _controller;
+
+    public ApplicationTests()
+    {
+        _mockReportService = new Mock<IReportService>();
+        _controller = new ReportController(_mockReportService.Object);
+    }
+
+    [Fact]
+    public async Task GetReportDetails_ReturnsOkResult_WithReport()
+    {
+        // Arrange
+        var reportId = Guid.NewGuid();
+        var report = new ReportDto { Id = reportId };
+        _mockReportService.Setup(service => service.GetReportDetailsAsync(reportId)).ReturnsAsync(report);
+
+        // Act
+        var result = await _controller.GetReportDetails(reportId);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<ReportDto>(okResult.Value);
+        Assert.Equal(reportId, returnValue.Id);
+    }
+
+    [Fact]
+    public async Task GetReportDetails_ReturnsNotFound_WhenReportIsNull()
+    {
+        // Arrange
+        var reportId = Guid.NewGuid();
+        _mockReportService.Setup(service => service.GetReportDetailsAsync(reportId)).ReturnsAsync((ReportDto)null);
+
+        // Act
+        var result = await _controller.GetReportDetails(reportId);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public async Task GetAllReports_ReturnsOkResult_WithListOfReports()
+    {
+        // Arrange
+        var reports = new List<ReportDto> { new ReportDto { Id = Guid.NewGuid() } };
+        _mockReportService.Setup(service => service.GetAllReportsAsync()).ReturnsAsync(reports);
+
+        // Act
+        var result = await _controller.GetAllReports();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<List<ReportDto>>(okResult.Value);
+        Assert.Single(returnValue);
+    }
+}
